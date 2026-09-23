58041c3d982bcf74a4143b8c61fcae28804e52ac
Tolga Y. Ozudogru <tolga@hetge.com>
2026-09-23 21:52:33 +0400

    chore: update manifests for v0.4.1

--- manifests/chocolatey/tools/chocolateyinstall.ps1
+++ manifests/chocolatey/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
-    url64bit       = 'https://github.com/tolgamorf/env2op-cli/releases/download/v0.4.0/env2op-windows-x64.zip'
-    checksum64     = '69445ABFB762D359CF079DCF419CB1A0EC162F0020F627CD0EDB00A2F452DE1E'
+    url64bit       = 'https://github.com/tolgamorf/env2op-cli/releases/download/v0.4.1/env2op-windows-x64.zip'
+    checksum64     = '06FA21BBFE9B759C56D726585EDB33AEC7FAA9D42BC014C8CD255054A402DD9A'
     checksumType64 = 'sha256'
 }
 
--- manifests/homebrew/env2op-cli.rb
+++ manifests/homebrew/env2op-cli.rb
@@ -1,8 +1,8 @@
 class Env2opCli < Formula
   desc "Push .env files to 1Password and pull them back"
   homepage "https://github.com/tolgamorf/env2op-cli"
-  url "https://registry.npmjs.org/@tolgamorf/env2op-cli/-/env2op-cli-0.4.0.tgz"
-  sha256 "7f1f0b6cdbd46cff895bdbe4dd156e8cd19b56444c1617e57707966b3e498aee"
+  url "https://registry.npmjs.org/@tolgamorf/env2op-cli/-/env2op-cli-0.4.1.tgz"
+  sha256 "2500b7862bc828adf6d140a6f69373acf6a57f3dfada3d6de1abb2ff721cddfe"
   license "MIT"
 
   depends_on "node"
